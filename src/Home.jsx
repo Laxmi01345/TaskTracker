c1579b55350723e4046340a545b91d61aaa614a9
laxmi013 <laxmiray013@gmail.com>
2024-02-23 17:23:25 +0530

    Initial commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom' 
 import './App.css'
-import Todo from './todopage'
+
 
 const Home=()=> {
   let arr = ["A To-do list is a love letter to your future self.", "Yor mind is for having ideas, not holding them.", "The power of To-do list lies in its ability to transform intensions into actions.", "Your Todo list is the compass in the chaos, guiding you to success.", "A to-do list turns your abstract goals into actionable tasks.","The simple act of making a todo list transforms vague aspirations into concrete steps toward success."]
